ff9cc11e90a0d0a7f6437166471debf7fba576c0
Aymen Idouahman <aymanidou@gmail.com>
2023-10-15 22:33:37 +0100

    user.py

--- models/user.py
+++ models/user.py
@@ -5,9 +5,8 @@ from models.base_model import BaseModel
 
 class User(BaseModel):
     """User class"""
-    def __init__(self):
-        """initiation"""
-        self.email = ""
-        self.password = ""
-        self.first_name = ""
-        self.last_name = ""
+
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
